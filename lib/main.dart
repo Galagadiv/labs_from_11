f3b6a48de17d27b3bb4e06c5f1345ee21f11a23a
Galagadiv <sobolia37@gmail.com>
2024-11-24 22:44:19 +0200

    rework with Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,38 @@
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Text Preview App',
+//       theme: ThemeData(primarySwatch: Colors.blue),
+//       home: HomeScreen(),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import './widgets/HomeScreen.dart';
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ColorProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Text Preview App',
-      theme: ThemeData(primarySwatch: Colors.blue),
       home: HomeScreen(),
     );
   }
--- lib/widgets/HomeScreen.dart
+++ lib/widgets/HomeScreen.dart
@@ -1,21 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  final TextEditingController _textController = TextEditingController();
+class ColorProvider with ChangeNotifier {
   double _redColor = 0;
   double _greenColor = 0;
   double _blueColor = 0;
 
+  double get redColor => _redColor;
+  double get greenColor => _greenColor;
+  double get blueColor => _blueColor;
+
+  void updateRedColor(double value) {
+    _redColor = value;
+    notifyListeners();
+  }
+
+  void updateGreenColor(double value) {
+    _greenColor = value;
+    notifyListeners();
+  }
+
+  void updateBlueColor(double value) {
+    _blueColor = value;
+    notifyListeners();
+  }
+}
+
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Lab 11'),
+        title: const Text('Lab 11 with Provider'),
         centerTitle: true,
         backgroundColor: const Color.fromRGBO(120, 66, 245, 0.4),
       ),
@@ -25,75 +41,58 @@ class _HomeScreenState extends State<HomeScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
+            // Color preview box
+            Center(
+              child: Container(
                 height: 150,
                 width: 150,
-                margin: EdgeInsets.only(bottom: 100),
-                color: Color.fromRGBO(_redColor.toInt(), _greenColor.toInt(), _blueColor.toInt(), 1),
-              ),]
+                margin: const EdgeInsets.only(bottom: 100),
+                color: Color.fromRGBO(
+                  context.watch<ColorProvider>().redColor.toInt(),
+                  context.watch<ColorProvider>().greenColor.toInt(),
+                  context.watch<ColorProvider>().blueColor.toInt(),
+                  1,
+                ),
+              ),
             ),
 
-            Text('Red: ${_redColor.toStringAsFixed(0)}'),
-            Row(
-              children: [
-                Expanded(
-                  child: Slider(
-                    value: _redColor,
-                    min: 0,
-                    max: 255,
-                    divisions: 255,
-                    label: _redColor.round().toString(),
-                    onChanged: (double value) {
-                      setState(() {
-                        _redColor = value;
-                      });
-                    },
-                  ),
-                ),
-              ],
+            // Red color slider
+            Text('Red: ${context.watch<ColorProvider>().redColor.toStringAsFixed(0)}'),
+            Slider(
+              value: context.watch<ColorProvider>().redColor,
+              min: 0,
+              max: 255,
+              divisions: 255,
+              label: context.watch<ColorProvider>().redColor.round().toString(),
+              onChanged: (double value) {
+                context.read<ColorProvider>().updateRedColor(value);
+              },
             ),
 
-            Text('Green: ${_greenColor.toStringAsFixed(0)}'),
-            Row(
-              children: [
-                Expanded(
-                  child: Slider(
-                    value: _greenColor,
-                    min: 0,
-                    max: 255,
-                    divisions: 255,
-                    label: _greenColor.round().toString(),
-                    onChanged: (double value) {
-                      setState(() {
-                        _greenColor = value;
-                      });
-                    },
-                  ),
-                ),
-              ],
+            // Green color slider
+            Text('Green: ${context.watch<ColorProvider>().greenColor.toStringAsFixed(0)}'),
+            Slider(
+              value: context.watch<ColorProvider>().greenColor,
+              min: 0,
+              max: 255,
+              divisions: 255,
+              label: context.watch<ColorProvider>().greenColor.round().toString(),
+              onChanged: (double value) {
+                context.read<ColorProvider>().updateGreenColor(value);
+              },
             ),
 
-            Text('Blue: ${_blueColor.toStringAsFixed(0)}'),
-            Row(
-              children: [
-                Expanded(
-                  child: Slider(
-                    value: _blueColor,
-                    min: 0,
-                    max: 255,
-                    divisions: 255,
-                    label: _blueColor.round().toString(),
-                    onChanged: (double value) {
-                      setState(() {
-                        _blueColor = value;
-                      });
-                    },
-                  ),
-                ),
-              ],
+            // Blue color slider
+            Text('Blue: ${context.watch<ColorProvider>().blueColor.toStringAsFixed(0)}'),
+            Slider(
+              value: context.watch<ColorProvider>().blueColor,
+              min: 0,
+              max: 255,
+              divisions: 255,
+              label: context.watch<ColorProvider>().blueColor.round().toString(),
+              onChanged: (double value) {
+                context.read<ColorProvider>().updateBlueColor(value);
+              },
             ),
           ],
         ),
